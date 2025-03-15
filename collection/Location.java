297291fa036ab799438918e7eb111708825e5de8
Shu'aibu Idris <idrisshuaibushubi1505@yahoo.com>
2025-03-15 17:56:45 +0300

    Changed 'jakarta' dependency

--- collection/Location.java
+++ collection/Location.java
@@ -1,8 +1,8 @@
 package org.example.collection;
 
 
-import jakarta.xml.bind.annotation.XmlAccessType;
-import jakarta.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Location {
