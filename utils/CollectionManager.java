fe8ca671265080b2e4bcf39d385a73ad33d5f782
Shu'aibu Idris <idrisshuaibushubi1505@yahoo.com>
2025-03-15 17:59:23 +0300

    Changed 'jakarta' dependency

--- utils/CollectionManager.java
+++ utils/CollectionManager.java
@@ -2,10 +2,10 @@ package org.example.utils;
 
 
 
-import jakarta.xml.bind.annotation.XmlAccessType;
-import jakarta.xml.bind.annotation.XmlAccessorType;
-import jakarta.xml.bind.annotation.XmlElement;
-import jakarta.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import org.example.collection.*;
 
 import java.io.IOException;
