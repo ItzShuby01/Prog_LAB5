4739e826ea63ffcc221359b8eb408d71455624e1
Shu'aibu Idris <idrisshuaibushubi1505@yahoo.com>
2025-03-19 01:59:29 +0300

    Fixed exit command to gracefully terminate app without 'System.exit()'

--- utils/ConsoleManager.java
+++ utils/ConsoleManager.java
@@ -3,24 +3,31 @@ package org.example.utils;
 import java.util.Scanner;
 
 public class ConsoleManager {
-    private final CommandManager commandManager;
-    private final Scanner scanner;
+    private CommandManager commandManager;
+    private Scanner scanner;
+    private boolean running = true;
 
+    public ConsoleManager() {}
 
-    public ConsoleManager(CommandManager commandManager) {
+    public void setCommandManager(CommandManager commandManager) {
         this.commandManager = commandManager;
         this.scanner = new Scanner(System.in);
     }
 
+    public void stop() {
+        running = false;
+    }
+
     public void interactiveMode() {
-        System.out.println("WELCOME TO THE PERSON COLLECTION APP \n");
+        System.out.println("WELCOME TO THE PERSON COLLECTION APP");
         System.out.println("Enter 'help' for manual, 'exit' to stop the program");
 
-        while (true) {
+        while (running) {
             System.out.print("Enter command> ");
-            String command = scanner.nextLine();
-
-            commandManager.executeCommand(command);
+            String command = scanner.nextLine().trim();
+            if (!command.isEmpty()) {
+                commandManager.executeCommand(command);
+            }
         }
     }
 }
