4ca0ad3cdc8bc334e6a79393fb93d826dad91f41
Shu'aibu Idris <idrisshuaibushubi1505@yahoo.com>
2025-03-19 02:01:27 +0300

    Fix exit command to gracefully terminate app without System.exit()

--- commands/Exit.java
+++ commands/Exit.java
@@ -1,14 +1,23 @@
 package org.example.commands;
 
-public class Exit implements Command{
+import org.example.commands.Commands;
+import org.example.utils.ConsoleManager;
+
+public class Exit implements Commands {
+    private final ConsoleManager consoleManager;
+
+    public Exit(ConsoleManager consoleManager) {
+        this.consoleManager = consoleManager;
+    }
 
     @Override
     public void execute() {
-        System.out.println("EXITING THE APP, SEE YOU AGAIN LATER !!!");
-        System.exit(1);
+        System.out.println("Exiting the app. See you later!");
+        consoleManager.stop();
     }
 
     @Override
     public void execute(String arg) {
+        execute();
     }
 }
