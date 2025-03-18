51c848e8ec7d0a34ae801cc3859df64f9bdb2572
Shu'aibu Idris <idrisshuaibushubi1505@yahoo.com>
2025-03-19 02:02:48 +0300

    Fix exit command to gracefully terminate app without System.exit()

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ package org.example;
 
 import org.example.utils.*;
 
-
 public class Main {
 
 
@@ -13,10 +12,10 @@ public class Main {
             FileManager fileManager = new FileManager(collectionManager);
 
             //setting environmental variables.
-            //if you want to set environmental variables in your IDE, then set it up 
+            //if you want to set environmental variables in your IDE, then set it up
             String fileName = System.getenv("COLLECTION_FILE_PATH");
 
-            if (fileName == null || fileName.isEmpty()) {
+            if (fileName == null) {
                 System.out.println("Environment variable COLLECTION_FILE_PATH is not set or is empty.");
                 return;
             }
@@ -24,20 +23,21 @@ public class Main {
             // 1. for environmental variable
             fileManager.loadCollectionFromXml(fileName);
 
-             //2. If you don’t want to use environmenatl variables in your IDE , uncomment the line below,  and then comment/remove lines 16 to 24
-//            fileManager.loadCollectionFromXml("storage.xml");
+            //2. If you don’t want to use environmental variables in your IDE , uncomment the line below,  and then comment/remove lines 16 to 24
+          // fileManager.loadCollectionFromXml("storage.xml");
 
             //3. Uncomment the line below if you want to pass the name of the file directly in the CLI( terminal)
-           //fileManager.loadCollectionFromXml(args[0]);
-                //Type this on terminal if you want to use the above command (java -jar java5-1.0-SNAPSHOT.jar ../storage.xml)
+            //fileManager.loadCollectionFromXml(args[0]);
+            //Type this on terminal if you want to use the above command (java -jar java5-1.0-SNAPSHOT.jar ../storage.xml)
 
-            CommandManager commandManager = new CommandManager(collectionManager);
-            ConsoleManager consoleManager = new ConsoleManager(commandManager);
+            ConsoleManager consoleManager = new ConsoleManager();
+            CommandManager commandManager = new CommandManager(collectionManager, consoleManager);
+            consoleManager.setCommandManager(commandManager);
 
             consoleManager.interactiveMode();
 
         } catch (Exception e) {
-             System.err.println("Error: " + e.getMessage());
+            System.err.println("Error: " + e.getMessage());
         }
     }
 }
