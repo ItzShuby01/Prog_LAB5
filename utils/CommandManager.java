18440ad294f15278c637600cff6da5d28e0144aa
Shu'aibu Idris <idrisshuaibushubi1505@yahoo.com>
2025-03-20 01:49:56 +0300

    Dynamically generate available commands ('Help' command)
    Using CommandManager metadata , all the available commands and their description are dynamically generated and fixed dependency injection

--- utils/CommandManager.java
+++ utils/CommandManager.java
@@ -12,6 +12,7 @@ public class CommandManager {
     private final LinkedList<String> commandHistory = new LinkedList<>();
     private final HashMap<String, Consumer<String>> commandMap = new HashMap<>();
     private final Set<String> argumentRequiredCommands = new HashSet<>();
+    private final HashMap<String, String> commandDescriptionMap = new HashMap<>();
 
     //Constructor for CommandManager  with reference to  CollectionManager.
     public CommandManager(CollectionManager collectionManager, ConsoleManager consoleManager) {
@@ -22,7 +23,7 @@ public class CommandManager {
 
 
     private void initializeCommands() {
-       //make sure commands that require arguments are passed arguments
+       //Argument validation - make sure commands that require arguments are passed arguments
         argumentRequiredCommands.add("update");
         argumentRequiredCommands.add("remove_by_id");
         argumentRequiredCommands.add("count_by_location");
@@ -30,25 +31,66 @@ public class CommandManager {
         argumentRequiredCommands.add("execute_script");
 
         //Initializes all available commands and stores them in a Command map.
-        commandMap.put("help", arg -> new Help().execute());
+        commandMap.put("help", arg -> new Help(this).execute());
+        commandDescriptionMap.put("help", "help: display help on available commands");
+
         commandMap.put("exit", arg -> new Exit(consoleManager).execute());
+        commandDescriptionMap.put("exit", "exit: exit the program (without saving to file)");
+
         commandMap.put("info", arg -> new Info(collectionManager).execute());
+        commandDescriptionMap.put("info", "info: print collection information (type, initialization date, number of elements, etc.) to standard output");
+
         commandMap.put("show", arg -> new Show(collectionManager).execute());
+        commandDescriptionMap.put("show", "show: print all elements of the collection to standard output");
+
         commandMap.put("save", arg -> new Save(collectionManager).execute());
+        commandDescriptionMap.put("save", "save: save collection to file");
+
         commandMap.put("add", arg -> new Add(collectionManager).execute());
+        commandDescriptionMap.put("add", "add {element}: add a new item to the collection");
+
         commandMap.put("clear", arg -> new Clear(collectionManager).execute());
+        commandDescriptionMap.put("clear", "clear: clear collection");
+
         commandMap.put("history", arg -> new History(commandHistory).execute());
+        commandDescriptionMap.put("history", "history: print the last 7 commands (without their arguments)");
+
+
         commandMap.put("max_by_id", arg -> new MaxById(collectionManager).execute());
+        commandDescriptionMap.put("max_by_id", "max_by_id: output any object from the collection whose id field value is maximum");
+
         commandMap.put("average_of_height", arg -> new AverageOfHeight(collectionManager).execute());
+        commandDescriptionMap.put("average_of_height","average_of_height: output the average height field value for all elements in a collection");
+
         commandMap.put("add_if_max", arg -> new AddIfMax(collectionManager).execute());
+        commandDescriptionMap.put("add_if_max", "add_if_max {element}: add a new element to a collection if its value is greater than the value of the largest element in that collection");
+
+
+
 
         // Adding commands that require arguments
         commandMap.put("update", arg -> new Update(collectionManager).execute(arg));
+        commandDescriptionMap.put("update", "update id {element}: update the value of the collection element whose id is equal to the given one");
+
         commandMap.put("remove_by_id", arg -> new RemoveById(collectionManager).execute(arg));
+        commandDescriptionMap.put("remove_by_id", "remove_by_id id: remove an element from a collection by its id");
+
         commandMap.put("count_by_location", arg -> new CountByLocation(collectionManager).execute(arg));
+        commandDescriptionMap.put("count_by_location", "count_by_location location: output the number of elements whose location field value is equal to the specified one");
+
         commandMap.put("remove_lower", arg -> new RemoveLower(collectionManager).execute(arg));
+        commandDescriptionMap.put("remove_lower", "remove_lower {element}: remove all elements from the collection that are less than the specified number");
+
         commandMap.put("execute_script", this::readCommandsFromScript);
+        commandDescriptionMap.put("execute_script", "execute_script file_name: read and execute the script from the specified file. The script contains commands in the same form in which the user enters them in interactive mode");
+
     }
+
+    // Getter for commands descriptions map
+    public Map<String, String> getCommandDescriptionMap() {
+        return new HashMap<>(commandDescriptionMap);
+    }
+
 
       //Executes the given command by retrieving it from the command map.
       //If the command is not found, it prints an error message.
@@ -62,12 +104,12 @@ public class CommandManager {
               if (argumentRequiredCommands.contains(commandName)) {
                   if (arg.isEmpty()) {
                       System.out.println("Error: Command '" + commandName + "' requires an argument.");
-                      return; 
+                      return;
                   }
               } else {
                   if (!arg.isEmpty()) {
                       System.out.println("Error: Command '" + commandName + "' does not take an argument.");
-                      return; 
+                      return;
                   }
               }
 
@@ -96,4 +138,5 @@ public class CommandManager {
             executeCommand(command);
         }
     }
+
 }
