51ab17f82edae27180f4dbd5b53144d5ecd002b6
Shu'aibu Idris <idrisshuaibushubi1505@yahoo.com>
2025-03-22 16:53:31 +0300

    Add 'close' method in the interface

--- utils/IOService.java
+++ utils/IOService.java
@@ -1,8 +1,10 @@
 package org.example.utils;
 
-public interface IOService {
+public interface IOService extends AutoCloseable {
     void print(String message);
     String readLine(String prompt);
     int readInt(String prompt);
     double readDouble(String prompt);
+    @Override
+    void close();
 }
