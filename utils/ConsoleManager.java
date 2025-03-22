f4d81acf983ca94a8e4ae96c57c62d3866d61180
Shu'aibu Idris <idrisshuaibushubi1505@yahoo.com>
2025-03-23 02:10:08 +0300

    Fix resource leak by closing Scanner

--- utils/ConsoleManager.java
+++ utils/ConsoleManager.java
@@ -4,14 +4,12 @@ import java.util.Scanner;
 
 public class ConsoleManager {
     private CommandManager commandManager;
-    private Scanner scanner;
     private boolean running = true;
 
     public ConsoleManager() {}
 
     public void setCommandManager(CommandManager commandManager) {
         this.commandManager = commandManager;
-        this.scanner = new Scanner(System.in);
     }
 
     public void stop() {
@@ -20,13 +18,15 @@ public class ConsoleManager {
 
     public void interactiveMode() {
         System.out.println("WELCOME TO THE PERSON COLLECTION APP");
-        System.out.println("Enter 'help' for manual, 'exit' to stop the program");
+        System.out.println("Enter 'help' to see available commands, 'exit' to stop the program");
 
-        while (running) {
-            System.out.print("Enter command> ");
-            String command = scanner.nextLine().trim();
-            if (!command.isEmpty()) {
-                commandManager.executeCommand(command);
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (running) {
+                System.out.print("Enter command> ");
+                String command = scanner.nextLine().trim();
+                if (!command.isEmpty()) {
+                    commandManager.executeCommand(command);
+                }
             }
         }
     }
