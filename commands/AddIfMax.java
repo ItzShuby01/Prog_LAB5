902f31b5d3b5275ae2af47d1c07b6fe04838196f
Shu'aibu Idris <idrisshuaibushubi1505@yahoo.com>
2025-03-24 18:40:59 +0300

    refactor I/O concerns in AddIfMax.java

--- commands/AddIfMax.java
+++ commands/AddIfMax.java
@@ -2,25 +2,31 @@ package org.example.commands;
 
 import org.example.utils.CollectionManager;
 import org.example.collection.Person;
+import org.example.utils.IOService;
+import org.example.utils.PersonIOService;
 
 public class AddIfMax implements Command {
     private final CollectionManager collectionManager;
+    private final PersonIOService personIOService;
+    private final IOService ioService;
 
-    public AddIfMax(CollectionManager collectionManager) {
+    public AddIfMax(CollectionManager collectionManager, PersonIOService personIOService, IOService ioService) {
         this.collectionManager = collectionManager;
+        this.personIOService = personIOService;
+        this.ioService = ioService;
     }
 
     @Override
     public void execute() {
-        System.out.println("Creating a new person to check if it's the max by height...");
-        Person newPerson = collectionManager.readPerson();
+        ioService.print("Creating a new person to check if it's the max by height...");
+        Person newPerson = personIOService.readPerson();
         double currentMaxHeight = collectionManager.getMaxHeight();
 
         if (newPerson.getHeight() > currentMaxHeight) {
             collectionManager.add(newPerson);
-            System.out.println("New person added to collection as their height is greater than the current maximum.");
+            ioService.print("New person added to collection as their height is greater than the current maximum.");
         } else {
-            System.out.println("New person's height is not greater than the current maximum. Not added to collection.");
+            ioService.print("New person's height is not greater than the current maximum. Not added to collection.");
         }
     }
 }
