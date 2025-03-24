008c892fb235cf9687885501946fd94cc13c8b97
Shu'aibu Idris <idrisshuaibushubi1505@yahoo.com>
2025-03-24 18:45:48 +0300

    refactor I/O concerns in Info.java

--- commands/Info.java
+++ commands/Info.java
@@ -1,16 +1,23 @@
 package org.example.commands;
 
 import org.example.utils.CollectionManager;
+import org.example.utils.CollectionManager.InfoData;
+import org.example.utils.IOService;
 
 public class Info implements Command {
     private final CollectionManager collectionManager;
+    private final IOService ioService;
 
-    public Info(CollectionManager collectionManager) {
+    public Info(CollectionManager collectionManager, IOService ioService) {
         this.collectionManager = collectionManager;
+        this.ioService = ioService;
     }
 
     @Override
     public void execute() {
-        collectionManager.info();
+        InfoData info = collectionManager.getInfo();
+        ioService.print("Collection Type: " + info.getCollectionType());
+        ioService.print("Initialization Date: " + info.getInitializationDate());
+        ioService.print("Number of persons in collection: " + info.getElementCount());
     }
 }
