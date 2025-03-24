bccff71f781a1f78665a09e4e5f42f4f33131e56
Shu'aibu Idris <idrisshuaibushubi1505@yahoo.com>
2025-03-24 18:44:10 +0300

    refactor I/O concerns in Exit.java

--- commands/Exit.java
+++ commands/Exit.java
@@ -1,17 +1,20 @@
 package org.example.commands;
 
 import org.example.utils.ConsoleManager;
+import org.example.utils.IOService;
 
 public class Exit implements Command {
     private final ConsoleManager consoleManager;
+    private final IOService ioService;
 
-    public Exit(ConsoleManager consoleManager) {
+    public Exit(ConsoleManager consoleManager, IOService ioService) {
         this.consoleManager = consoleManager;
+        this.ioService =ioService;
     }
 
     @Override
     public void execute() {
-        System.out.println("Exiting the app. See you again later!!!");
+        ioService.print("Exiting the App. See You Again Later!!!");
         consoleManager.stop();
     }
 }
