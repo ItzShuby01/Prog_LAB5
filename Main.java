414ca46f2850fdbcf9ac0735fa9158745684eb45
Shu'aibu Idris <idrisshuaibushubi1505@yahoo.com>
2025-03-24 18:34:35 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,9 +6,10 @@ public class Main {
 
 
     public static void main(String[] args) {
-        try(ConsoleIOService ioService = new ConsoleIOService()){
-            CollectionManager collectionManager = new CollectionManager(ioService);
+        try ( ConsoleIOService ioService = new ConsoleIOService()){
+            CollectionManager collectionManager = new CollectionManager();
             FileManager fileManager = new FileManager(collectionManager);
+            PersonIOService personIOService= new PersonIOService(ioService, collectionManager);
 
             //setting environmental variables.
             //if you want to set environmental variables in your IDE, then set it up
@@ -23,14 +24,14 @@ public class Main {
             fileManager.loadCollectionFromXml(fileName);
 
             //2. If you don’t want to use environmental variables in your IDE , uncomment the line below,  and then comment/remove lines 16 to 24
-          // fileManager.loadCollectionFromXml("storage.xml");
+            //fileManager.loadCollectionFromXml("storage.xml");
 
             //3. Uncomment the line below if you want to pass the name of the file directly in the CLI( terminal)
             //fileManager.loadCollectionFromXml(args[0]);
             //Type this on terminal if you want to use the above command (java -jar java5-1.0-SNAPSHOT.jar ../storage.xml)
 
             ConsoleManager consoleManager = new ConsoleManager();
-            CommandManager commandManager = new CommandManager(collectionManager, consoleManager);
+            CommandManager commandManager = new CommandManager(collectionManager, consoleManager,personIOService, ioService);
             consoleManager.setCommandManager(commandManager);
 
             consoleManager.interactiveMode();
@@ -38,5 +39,6 @@ public class Main {
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
         }
+
     }
 }
