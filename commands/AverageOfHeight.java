4818a4e58f3b839f8b78015f7602cde5c1969b69
Shu'aibu Idris <idrisshuaibushubi1505@yahoo.com>
2025-03-24 18:41:38 +0300

    refactor I/O concerns in AverageOfHeight.java

--- commands/AverageOfHeight.java
+++ commands/AverageOfHeight.java
@@ -1,16 +1,25 @@
 package org.example.commands;
 
 import org.example.utils.CollectionManager;
+import org.example.utils.IOService;
 
 public class AverageOfHeight implements Command {
     private final CollectionManager collectionManager;
+    private final IOService ioService;
 
-    public AverageOfHeight(CollectionManager collectionManager) {
+    public AverageOfHeight(CollectionManager collectionManager, IOService ioService) {
         this.collectionManager = collectionManager;
+        this.ioService = ioService;
     }
 
     @Override
     public void execute() {
-        collectionManager.printAverageHeight();
+        try {
+            double average = collectionManager.getAverageHeight();
+            ioService.print(String.format("Average height: %.2f", average));
+        } catch (IllegalStateException e) {
+            ioService.print(e.getMessage());
+        }
     }
+
 }
