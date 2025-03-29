2fad5c29813a66b4589465ce78cdca25844906b0
Shu'aibu Idris <idrisshuaibushubi1505@yahoo.com>
2025-03-29 23:37:12 +0300

    Delete LAB5 copy directory

--- LAB5 copy/src/main/java/org/example/collection/HairColor.java
+++
@@ -1,8 +0,0 @@
-package org.example.collection;
-
-public enum HairColor {
-    GREEN,
-    BLUE,
-    YELLOW,
-    BROWN;
-}
--- LAB5 copy/src/main/java/org/example/commands/Command.java
+++
@@ -1,9 +0,0 @@
-package org.example.commands;
-
-public interface Command {
-    default void execute() {
-    }
-
-    default void execute(String arg) {
-    }
-}
